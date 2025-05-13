3a1c9f9f698b0a70a090a2b2afb7f006358c1f00
Ktsu0 <gabrielwag971@gmail.com>
2025-05-12 22:00:17 -0300

    cards animations

--- portifolio/js/cards.js
+++ portifolio/js/cards.js
@@ -1,18 +1,21 @@
+import { cards_bd } from "./cards_bd.js"
+
+
 export function createCards(){
     let cards_section = document.getElementById('cards')
 
-for(let i = 0; i < 40; i++ ){
+for(let i = 0; i < cards_bd.length; i++ ){
     let div_cards = document.createElement("div")
     div_cards.className = "card center_alignment column"
 
-    let i_cards = document.createElement("i")
-    i_cards.className = "icon fa-brands fa-instagram icontheer icon"
+    let i_cards = document.createElement("img")
+    i_cards.src = cards_bd[i].img
 
     let text_cards = document.createElement("h1")
-    text_cards.textContent = "Nome do Produto"
+    text_cards.textContent = cards_bd[i].name
 
-    let valor_cards = document.createElement("h4")
-    valor_cards.textContent = "R$ 16,00"
+    let valor_cards = document.createElement("h3")
+    valor_cards.textContent = cards_bd[i].language
 
     div_cards.appendChild(i_cards)
     div_cards.appendChild(text_cards)
--- portifolio/js/projects.js
+++ portifolio/js/projects.js
@@ -3,9 +3,11 @@ import { precione_btn } from "./btn.js"
 import { createCards } from "./cards.js"
 
 import { switchMenu } from "./menu.js"
+import { switchBanner } from "./banner.js";
 
 
 window.switchMenu = switchMenu
+window.switchBanner = switchBanner
 
 document.addEventListener("DOMContentLoaded", ()=>{
     createHeader() 
